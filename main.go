4788ef33145b26a88072b1ff14c6be84d033a2b7
LucaDiFiori <150525008+LucaDiFiori@users.noreply.github.com>
2025-02-25 15:25:17 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 		fmt.Println("bella ciao ! 3")
+// }
 package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-		fmt.Println("bella ciao ! 3")
+	for {
+		fmt.Println("bella ciao ! 2")
+		time.Sleep(3 * time.Second)
+	}
 }
